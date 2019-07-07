c480275c58c1a4364c5ae80e7eafaff4e8d4c702
rasalghul <liao13668009346@163.com>
2019-07-07 16:04:32 +0800

    Initial commit

---
+++ app/src/main/java/com/bytedance/camera/demo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.bytedance.camera.demo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_EXTERNAL_CUSTOMER = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_picture).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, TakePictureActivity.class));
+        });
+
+        findViewById(R.id.btn_camera).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, RecordVideoActivity.class));
+        });
+
+        findViewById(R.id.btn_custom).setOnClickListener(v -> {
+            //todo 在这里申请相机、麦克风、存储的权限
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO
+                },REQUEST_EXTERNAL_CUSTOMER);
+            } else {
+                startActivity(new Intent(MainActivity.this, CustomCameraActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_CUSTOMER: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startActivity(new Intent(MainActivity.this, CustomCameraActivity.class));
+                }
+                else {
+                    Toast.makeText(this,"打开失败",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/bytedance/camera/demo/TakePictureActivity.java
@@ -0,0 +1,115 @@
+package com.bytedance.camera.demo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bytedance.camera.demo.utils.Utils;
+
+import java.io.File;
+
+public class TakePictureActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private File imgFile;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 101;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+        imageView = findViewById(R.id.img);
+        findViewById(R.id.btn_picture).setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(TakePictureActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(TakePictureActivity.this,
+                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                //todo 在这里申请相机、存储的权限
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA
+                },REQUEST_EXTERNAL_STORAGE);
+            } else {
+                takePicture();
+            }
+        });
+
+    }
+
+
+
+    private void takePicture() {
+        //todo 打开相机
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imgFile = Utils.getOutputMediaFile(Utils.MEDIA_TYPE_IMAGE);
+        if(imgFile != null){
+            Uri fileUri = FileProvider.getUriForFile(this,"com.bytedance.camera.demo",imgFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            setPic();
+        }
+    }
+
+    private void setPic() {
+        //todo 根据imageView裁剪
+        int targetW = imageView.getWidth();
+        int targetH  = imageView.getHeight();
+        //todo 根据缩放比例读取文件，生成Bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imgFile.getAbsolutePath(),bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW/targetW,photoH/targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bmp = BitmapFactory.decodeFile(imgFile.getAbsolutePath(),bmOptions);
+
+        //todo 如果存在预览方向改变，进行图片旋转
+        Utils.rotateImage(bmp,imgFile.getAbsolutePath());
+        imageView.setImageBitmap(bmp);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                //todo 判断权限是否已经授予
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    takePicture();
+                }
+                else {
+                    Toast.makeText(this,"打开失败",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+}
